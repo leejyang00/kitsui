313a947f0f7111be900d012902b30c2f06d80bce
Jian Yang Lee <leejyang00@gmail.com>
2024-11-11 21:41:55 +1000

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,29 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+})
+
+// Register things for typesafety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
+
+// if (!rootElement.innerHTML) {
+//   const root = ReactDOM.createRoot(rootElement)
+//   root.render(<RouterProvider router={router} />)
+// }
